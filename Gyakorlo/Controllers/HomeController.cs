c07e38d30ee05efa759105e40c4408ab0c63b068
rostas.peter <rostas.peter@getcon.hu>
2025-03-17 19:33:16 +0100

    Add project files.

---
+++ Gyakorlo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Gyakorlo.Models;
+
+namespace Gyakorlo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Rolunk()
+    {
+        return View();
+    }
+
+    public IActionResult Utmutato()
+    {
+        return View();
+    }
+
+    public IActionResult Kapcsolat()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Gyakorlo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gyakorlo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
