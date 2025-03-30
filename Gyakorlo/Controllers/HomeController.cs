9ec36f641b6e081a9f9914bbd5986fa5ab0e8bd9
rostas.peter <rostas.peter@getcon.hu>
2025-03-30 14:27:51 +0200

    teszt

--- Gyakorlo/Controllers/HomeController.cs
+++ Gyakorlo/Controllers/HomeController.cs
@@ -36,6 +36,7 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
+        //teszt viktor
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
